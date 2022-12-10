a282a834a0e3faea655c2d916c65b881a78e6e20
Elton SV <el10savio96@gmail.com>
2022-12-10 03:38:25 +0100

    Init Commit

---
+++ zigzag/zigzag.go
@@ -0,0 +1,43 @@
+package zigzag
+
+// TODO:
+// - Add README.md
+// - Add Comments
+// - Add Benchmarks
+// - Substitute MUL/DIV By 2 With Bit Shifts
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Integer interface {
+	constraints.Integer
+}
+
+func Encode[T Integer](num T) T {
+	TZero := *new(T)
+
+	if num < TZero {
+		return 2*abs[T](num) - 1
+	}
+
+	return 2 * num
+}
+
+func Decode[T Integer](num T) T {
+	if num%2 != 0 {
+		return -(num + 1) / 2
+	}
+
+	return num / 2
+}
+
+func abs[T Integer](num T) T {
+	TZero := *new(T)
+
+	if num < TZero {
+		return -num
+	}
+
+	return num
+}
---
+++ zigzag/zigzag_test.go
@@ -0,0 +1,65 @@
+package zigzag
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncode(t *testing.T) {
+	testCases := []struct {
+		name             string
+		num              int
+		expectedEncoding int
+	}{
+		{"positive number", 8, 16},
+		{"positive number ii", 20, 40},
+		{"negative number", -8, 15},
+		{"zero", 0, 0},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			actualEncoding := Encode(tc.num)
+			assert.Equal(t, tc.expectedEncoding, actualEncoding)
+		})
+	}
+}
+
+func TestDecode(t *testing.T) {
+	testCases := []struct {
+		name             string
+		num              int
+		expectedDecoding int
+	}{
+		{"positive number", 28, 14},
+		{"positive number ii", 36, 18},
+		{"negative number", 33, -17},
+		{"negative number ii", 39, -20},
+		{"zero", 0, 0},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			actualDecoding := Decode(tc.num)
+			assert.Equal(t, tc.expectedDecoding, actualDecoding)
+		})
+	}
+}
+
+func FuzzEncode(f *testing.F) {
+	f.Fuzz(func(t *testing.T, num int) {
+		numEncoded := Encode(num)
+		numDecoded := Decode(numEncoded)
+
+		assert.Equal(t, num, numDecoded)
+
+		if num < 0 {
+			assert.True(t, numEncoded%2 == 1)
+		} else {
+			assert.True(t, numEncoded%2 == 0)
+		}
+	})
+}
