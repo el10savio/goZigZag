0e05f2e385b8ef7bac1df2ac78842f5b30902675
Elton SV <el10savio96@gmail.com>
2022-12-10 18:05:25 +0100

    Updated Package

---
+++ zigzag_test.go
@@ -0,0 +1,70 @@
+package zigzag_test
+
+import (
+	"testing"
+
+	"github.com/el10savio/zigzag"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncode(t *testing.T) {
+	testCases := []struct {
+		name             string
+		num              int
+		expectedEncoding int
+	}{
+		{"positive number", 8, 16},
+		{"positive number ii", 20, 40},
+		{"negative number", -8, 15},
+		{"zero", 0, 0},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			actualEncoding := zigzag.Encode(tc.num)
+			assert.Equal(t, tc.expectedEncoding, actualEncoding)
+		})
+	}
+}
+
+func TestDecode(t *testing.T) {
+	testCases := []struct {
+		name             string
+		num              int
+		expectedDecoding int
+	}{
+		{"positive number", 28, 14},
+		{"positive number ii", 36, 18},
+		{"negative number", 33, -17},
+		{"negative number ii", 39, -20},
+		{"zero", 0, 0},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			actualDecoding := zigzag.Decode(tc.num)
+			assert.Equal(t, tc.expectedDecoding, actualDecoding)
+		})
+	}
+}
+
+func FuzzEncode(f *testing.F) {
+	f.Fuzz(func(t *testing.T, num int) {
+		numEncoded := zigzag.Encode(num)
+		numDecoded := zigzag.Decode(numEncoded)
+
+		// ZigZag Decoding an Encoded number
+		// yields the orginal number
+		assert.Equal(t, num, numDecoded)
+
+		if num < 0 {
+			// If a number is negative the encoded number is odd
+			assert.True(t, numEncoded%2 == 1)
+		} else {
+			// If a number is positive the encoded number is even
+			assert.True(t, numEncoded%2 == 0)
+		}
+	})
+}
